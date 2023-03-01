13a07660782c28bd2cdd8ee4b4647e84ce9a3976
Jordi Jane <jordijane98@gmail.com>
2023-03-01 12:29:49 +0100

    get satelite positions

--- __main__.py
+++ __main__.py
@@ -1,10 +1,26 @@
-from go_to_api_n2yo import go_to_url, get_tle
+from go_to_api_n2yo import go_to_url, get_tle, get_satelit_position
 def main():
     go_to_url()
     satelite = get_tle()
+    print("Tle")
     print("Satelite ID:", satelite.sat_id)
     print("Satelite Name:", satelite.sat_name)
     print("Satelite Transactions Count:", satelite.transactions_count)
+    satelite2, positions = get_satelit_position()
+    print("Satelite info")
+    print("Satelite ID:", satelite2.sat_id)
+    print("Satelite Name:", satelite2.sat_name)
+    print("Satelite Transactions Count:", satelite2.transactions_count)
+    print("Satelito Positions")
+    print("satlatitude:", positions.satlatitude)
+    print("satlongitude:", positions.satlongitude)
+    print("sataltitude:", positions.sataltitude)
+    print("azimuth:", positions.azimuth)
+    print("elevation:", positions.elevation)
+    print("ra:", positions.ra)
+    print("dec:", positions.dec)
+    print("timestamp:", positions.timestamp)
+    print("eclipsed",positions.eclipsed)
 if __name__ == '__main__':
     # This code won't run if this file is imported.
     main()
--- go_to_api_n2yo.py
+++ go_to_api_n2yo.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 import requests
 from satelite import Satelite
 from typing import List
+from positions import Positions
 APIKEY = "E8SWFK-FDRUMV-SV6LN2-500F"
 
 def go_to_url():
@@ -24,3 +25,32 @@ def get_tle():
     satelite : Satelite = Satelite(sat_id, sat_name, transactions_count)
 
     return satelite
+
+def get_satelit_position():
+    response = requests.get("https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=E8SWFK-FDRUMV-SV6LN2-500F")
+    response.raise_for_status()
+
+    satelite_dict: dict = response.json()
+
+    info = satelite_dict.get("info")
+    sat_id = info.get("satid")
+    sat_name = info.get("satname")
+    transactions_count = info.get("transactionscount")
+
+    positions = satelite_dict.get("positions")
+    positions_dict = positions[0]
+    satlatitude = positions_dict.get("satlatitude")
+    satlongitude = positions_dict.get("satlongitude")
+    sataltitude = positions_dict.get("sataltitude")
+    azimuth = positions_dict.get("azimuth")
+    elevation = positions_dict.get("elevation")
+    ra = positions_dict.get("ra")
+    dec = positions_dict.get("dec")
+    timestamp = positions_dict.get("timestamp")
+    eclipsed = positions_dict.get("eclipsed")
+
+    satelite: Satelite = Satelite(sat_id, sat_name, transactions_count)
+
+    positions: Positions = Positions(satlatitude, satlongitude, sataltitude, azimuth, elevation, ra, dec, timestamp, eclipsed)
+
+    return satelite, positions
