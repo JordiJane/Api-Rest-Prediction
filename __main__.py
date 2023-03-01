2c1f8db12537a5bc1f21a2e26f720d12ac923893
Jordi Jane <jordijane98@gmail.com>
2023-03-01 12:33:55 +0100

    saltos de linia

--- __main__.py
+++ __main__.py
@@ -4,10 +4,12 @@ from go_to_api_n2yo import go_to_url, get_tle, get_satelit_position
 def main():
     go_to_url()
     satelite = get_tle()
+    print("\n")
     print("Tle")
     print("Satelite ID:", satelite.sat_id)
     print("Satelite Name:", satelite.sat_name)
     print("Satelite Transactions Count:", satelite.transactions_count)
+    print("\n")
     satelite2, positions = get_satelit_position()
     print("Satelite info")
     print("Satelite ID:", satelite2.sat_id)
